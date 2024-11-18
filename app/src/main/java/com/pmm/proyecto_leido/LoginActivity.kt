0c1ebe81603c58feeca989943f3731e262db2d37
Ana Vázquez de Ágredos <anazeuqzav@gmail.com>
2024-11-18 11:04:29 +0100

    Interfaz Login y mensaje de bienvenida

--- app/src/main/java/com/pmm/proyecto_leido/LoginActivity.kt
+++ app/src/main/java/com/pmm/proyecto_leido/LoginActivity.kt
@@ -10,9 +10,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    // Constantes estáticas de usuario y contraseña
+    // Constantes estáticas de usuario y contraseña para validación del login
     companion object {
-        const val MYUSER = "Anazeuqzav"
+        const val MYUSER = "AnaVazquez"
         const val MYPASS = "root"
     }
 
@@ -28,28 +28,41 @@ class LoginActivity : AppCompatActivity() {
         // Configurar el botón de login
         loginButton.setOnClickListener {
             val username = usernameEditText.text.toString().trim() // elimina los espacios
-            val password = passwordEditText.text.toString().trim() // elimina los espacios
+            val password = passwordEditText.text.toString().trim()
 
-            // Comprobación si los campos están vacíos
-            if (username.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Introduce un nombre de usuario y una contraseña", Toast.LENGTH_SHORT).show()
-              // Comprobación si el usuario es correcto
-            } else if (username != MYUSER) {
-                Toast.makeText(this, "Usuario incorrecto", Toast.LENGTH_SHORT).show();
-              // Comprobación si la contraseña es correcta
-            } else if (password != MYPASS) {
-                Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
-              // Comprobación si ambas credenciales son correctas
-            } else if (username == MYUSER && password == MYPASS) {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("USERNAME", username) // Pasa el nombre de usuario
-                startActivity(intent)
-                finish() // Finaliza LoginActivity
-            } else {
-                // Si se produce algun otro error con las credencias
-                Toast.makeText(this, "Error en las credenciales", Toast.LENGTH_SHORT).show()
+            when {
+                // Comprobación si los campos están vacíos
+                username.isEmpty() || password.isEmpty() -> {
+                    Toast.makeText(
+                        this,
+                        "Introduce un nombre de usuario y una contraseña",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                // Comprobación si el usuario es incorrecto
+                username != MYUSER -> {
+                    Toast.makeText(this, "Usuario incorrecto", Toast.LENGTH_SHORT).show()
+                }
+
+                // Comprobación si la contraseña es incorrecta
+                password != MYPASS -> {
+                    Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
+                }
+
+                // Comprobación si ambas credenciales son correctas
+                username == MYUSER && password == MYPASS -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("USERNAME", username) // Pasa el nombre de usuario
+                    startActivity(intent)
+                    finish() // Finaliza LoginActivity
+                }
+
+                // Si se produce algún otro error con las credenciales
+                else -> {
+                    Toast.makeText(this, "Error en las credenciales", Toast.LENGTH_SHORT).show()
+                }
             }
         }
-
     }
 }
